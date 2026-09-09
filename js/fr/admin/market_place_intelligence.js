298a8d042c7d116514048ebc8c1faf2cbcd7437c
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-09-09 14:14:00 +0300

    m a j marketplace > CEO

---
+++ js/fr/admin/market_place_intelligence.js
@@ -0,0 +1,72 @@
+creerCarte(
+    "Marketplace intelligence",
+    `
+    <p>Voici les éléments dans le marketplace</p>
+    <ol>
+        <li><h4>Historique des tâches:</h4></li>
+            <p>Il se trouve tout en haut à droite à côté sur titre de la page pour le triage par date et heure</p>
+            
+            <ul>
+                
+                <li>Aujourd'hui:</li>
+                <p>Montre les tâches faites depuis le marketplace aujourd'hui</p>
+
+                <li>7 jours:</li>
+                <p>Montre les tâches faites les 7 derniers jours sur le marketplace</p>
+
+                <li>30 jours:</li>
+                <p>Montre les tâches faites les 30 derniers jours sur le marketplace</p>
+
+                <img src="../../img/fr/admin/market_place/historique_date.png" alt="img_marketplace" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+                
+            </ul>
+        
+        
+        
+
+    
+        <li><h4>CEO:</h4></li>
+        <p>Tableau de bord spécialisé dans le marketplace</p>
+        
+            <ul>
+                
+                <li>Product Maturity:</li>
+                <p>KPI: indique la performance d'une annoce publiée</p>
+                <img src="../../img/fr/admin/market_place/product_maturity.png" alt="img_marketplace" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+                <li>Search Index Health:</li>
+                <p>Résume le nombre de produits indéxés, redirection vers search intelligente. "Search intelligente sera détaillé dans la partie marketplace > search"</p>
+                <img src="../../img/fr/admin/market_place/index_health.png" alt="img_marketplace" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+
+                <li>Santé générale:</li>
+                <p>Montre la moyenne des traitements conformes faits pour les Catalogues, Fournisseurs, Recherche IA, RFQ, SEO, Traductions, Ontologie</p>
+                <img src="../../img/fr/admin/market_place/sante_generale.png" alt="img_marketplace" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+                <li>Recherche IA / Sans résultat / Taux de réussite :</li>
+                <p>Résumé statistique de recherche avec l'ia et raccourci vers la partie "marketplace > search"</p>
+
+                <li>Produits consultés</li>
+                <p>Résumé statistique des produits consultés et raccourci vers la partie "marketplace > product"</p>
+
+
+                <li>RFQ créés / RFQ envoyés / RFQ acceptés</li>
+                <p>Résumé statistique des demandes de devis et raccourci vers la partie "marketplace > RFQ"</p>
+
+                <li>Fournisseurs actifs</li>
+                <p>Résumé statistique des fournisseurs actifs et raccourci vers la partie "marketplace > suppliers"</p>
+                <img src="../../img/fr/admin/market_place/ceo_raccourci.png" alt="img_marketplace" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+            </ul>
+        
+
+        
+
+            
+    </ol>
+    `
+);
--- js/fr/admin/tableau_bord.js
+++ js/fr/admin/tableau_bord.js
@@ -1,76 +1,146 @@
 creerCarte(
     "Tableau de bord",
     `
-    <p>Voici les éléments dans le tableau de bord</p>
+    <p>Voici les éléments du tableau de bord.</p>
     <ol>
-        <li><h4>A traiter maintenant:</h4></li>
+        <li><h4>A traiter maintenant :</h4></li>
         
-        <ol>
-            <h5>Ce sont les tâches à vérifier et traiter en priorité:</h5>
+        <ul>
+            <h5>Ce sont les tâches à vérifier et traiter en priorité :</h5>
             <li>Produit à valider</li>
-            <p>Il permet de vérifier le status d'un produit: "Actif" ou "A valider"</p>
+            <p>Il permet de vérifier le statut d'un produit : "Actif" ou "À valider".</p>
 
-            <li>Litiges ouverts:</li>
-            <p>Il permet de voir les réclamations. Redirection vers la page support et réclamation</p>
+            <li>Litiges ouverts :</li>
+            <p>Il permet de voir les réclamations. Redirection vers la page support et réclamation.</p>
 
-            <li>Modération:</li>
-            <p>Redirection vers la page des signalements effectués</p>
+            <li>Modération :</li>
+            <p>Redirection vers la page des signalements effectués.</p>
 
-            
-        </ol>
-        <img src="../../img/fr/admin/traiter_maintenant.png" alt="img_tableau_bord" style="width: 70%;
+            <img src="../../img/fr/admin/traiter_maintenant.png" alt="img_tableau_bord" style="width: 70%;
             border: 2px solid black; border-radius: 8px; padding: 5px;">
+            
+        </ul>
+        
         
         
 
     
-        <li><h4>Accès rapide:</h4></li>
+        <li><h4>Accès rapide :</h4></li>
         
-        <ol>
-            <h5>Liste des raccourcis:</h5>
-            
-            <li>Produit</li>
-            <p>Redirection vers la page produit</p>
+            <ul>
+                <h5>Liste des raccourcis :</h5>
+                
+                <li>Produit</li>
+                <p>Redirection vers la page produits.</p>
 
-            <li>Inbox fournisseur:</li>
-            <p>Rediection vers la page de boîte de réception des notifications pour les messages, demande RFQ et devis d'un fournisseur</p>
+                <li>Inbox fournisseur :</li>
+                <p>Redirection vers la page de boîte de réception des notifications pour les messages, les demandes RFQ et les devis d'un fournisseur.</p>
 
-            <li>Demande RFQ:</li>
-            <p>Redirection vers la page de demande de devis (RFQ) qui les liste</p>
+                <li>Demande RFQ :</li>
+                <p>Redirection vers la page de demande de devis (RFQ) qui les liste.</p>
 
-            <li>Commande:</li>
-            <p>Redirection vers la page des listes et les status de livraison d'une commande</p>
+                <li>Commande :</li>
+                <p>Redirection vers la page des listes et des statuts de livraison d'une commande.</p>
 
-            <li>Modération</li>
-            <p>Redirection vers la page des signalements effectués</p>
+                <li>Modération</li>
+                <p>Redirection vers la page des signalements effectués.</p>
 
-            <li>Modération</li>
-            <p>Redirection vers la liste des fournisseurs</p>
+                <li>Modération</li>
+                <p>Redirection vers la liste des fournisseurs.</p>
 
-            <li>Intelligence IA</li>
-            <p>Redirection vers l'interface de gestion de l'IA</p>
-        </ol>
-        <img src="../../img/fr/admin/access_rapide.png" alt="img_tableau_bord" style="width: 70%;
-            border: 2px solid black; border-radius: 8px; padding: 5px;">
+                <li>Intelligence IA</li>
+                <p>Redirection vers l'interface de gestion de l'IA.</p>
 
-
-        <li><h4>Plateforme:</h4></li>
+                <img src="../../img/fr/admin/access_rapide.png" alt="img_tableau_bord" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+            </ul>
         
-        <ol>
-            <li>Utilisateur:</li>
-            <p>Montre le nombre d'utilisateur et redirection vers la page utilisateur</p>
 
-            <li>Fournisseur:</li>
-            <p>Montre le nombre de fournisseur </p>
 
-            <li>Fabricant:</li>
-            <p>Redirection vers la page de la liste des fournisseurs</p>
+        <li><h4>Plateforme :</h4></li>
+        
+            <ul>
+                <li>Utilisateur :</li>
+                <p>Montre le nombre d'utilisateurs et redirection vers la page utilisateurs.</p>
 
-             <li>Catégorie:</li>
-            <p>Redirection vers la page qui liste la catégorie des produits disponibles</p>
+                <li>Fournisseur :</li>
+                <p>Montre le nombre de fournisseurs.</p>
+
+                <li>Fabricant :</li>
+                <p>Redirection vers la page de la liste des fournisseurs.</p>
+
+                <li>Catégorie :</li>
+                <p>Redirection vers la page qui liste les catégories des produits disponibles.</p>
+
+                
+            </ul>
+
+        <li><h4>Commerce :</h4></li>
+        
+            <ul>
+                <li>Produit :</li>
+                <p>Redirection vers la page "Produit".</p>
+
+                <li>Demande de devis :</li>
+                <p>Redirection vers la page "Demande de devis (RFQ)".</p>
+
+                <li>Commandes :</li>
+                <p>Redirection vers la page de la liste des commandes.</p>
+
+                <li>Chiffre d'affaires :</li>
+                <p>Redirection vers la page qui liste les paiements effectués.</p>
+
+                
+            </ul>
+
+        <li><h4>Qualité et conformité :</h4></li>
+        
+            <ul>
+                <li>Produit à valider :</li>
+                <p>Redirection vers la page "Produit".</p>
+
+                <li>KYC en attente :</li>
+                <p>Redirection vers la page qui montre les dossiers légaux (RC, NIF, NIS).</p>
+
+                <li>Litiges ouverts :</li>
+                <p>Redirection vers la page "Supports et réclamations".</p>
+
+                <li>Pays d'export :</li>
+                <p>Redirection vers la page qui liste les pays d'exportation.</p>
+
+                <img src="../../img/fr/admin/commerce.png" alt="img_tableau_bord" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+                
+            </ul>
 
             
-        </ol>
+
+        <li><h4>Graphiques :</h4></li>
+        
+            <ul>
+                <li>Chiffre d'affaires :</li>
+                <p>Montre une courbe des revenus des 6 derniers mois.</p>
+
+                <li>Utilisateur par type :</li>
+                <p>Diagramme en anneau concernant le nombre de comptes créés : en rouge le nombre d'acheteurs, en vert clair le nombre de super utilisateurs, en vert foncé le nombre de fournisseurs.</p>
+                <img src="../../img/fr/admin/chiffre_aff1.png" alt="img_tableau_bord" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+
+                <li>Commandes par statut :</li>
+                <p>Histogramme qui montre le nombre de commandes par statut : confirmée, en transit, livrée.</p>
+                <img src="../../img/fr/admin/chiffre_aff2.png" alt="img_tableau_bord" style="width: 70%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+                <li>Activité récente :</li>
+                <p>Montre les dernières activités sur la console d'administration.</p>
+                <img src="../../img/fr/admin/recentes.png" alt="img_tableau_bord" style="width: 40%;
+                border: 2px solid black; border-radius: 8px; padding: 5px;">
+
+                
+            </ul>
+
+            
     </ol>
     `
 );
