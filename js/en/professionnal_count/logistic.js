8339ca7ad7daacd0e96f4e47e4de242284163cac
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-09-13 08:23:15 +0300

    traitement final

--- js/en/professionnal_count/logistic.js
+++ js/en/professionnal_count/logistic.js
@@ -4,7 +4,26 @@ creerCarte(
     `
     <ol>
        
-        Waiting for the logistics section to be updated
+        <li><h4>Content</h4>
+
+
+        <ul>
+                        <li>Summarizes the preparation of an order</li>
+                        <li>Order → Preparation → Documents → Validation → Ready to ship → External export</li>
+                        <img src="../../img/fr/admin/professionnal_count/logistic.png" alt="dashboard_image">
+                    
+                    </ul>
+
+        <li><h4>Usage</h4>
+        
+            <ul>
+            
+                <li>Logistics: go to the bottom of the table > select a row to check > a processing window opens
+                </li>
+            </ul>
+        </li>
+
+
     </ol>
     `
 );
--- js/fr/professionnal_count/logistic.js
+++ js/fr/professionnal_count/logistic.js
@@ -3,8 +3,23 @@ creerCarte(
     "Logistiques",
     `
     <ol>
-       
-        En attente de la mise à jour de la partie logistique
+        <li><h4>Contenu</h4>
+
+        <ul>
+                        <li>Résume la préparation d'une commande</li>
+                        <li>Commande → Préparation → Documents → Validation → Prêt à expédier → Export externe</li>
+                        <img src="../../img/fr/admin/professionnal_count/logistic.png" alt="img_tableau_bord">
+                    
+                    </ul>
+
+        <li><h4>Utilisation</h4>
+        
+            <ul>
+               
+                <li>logistique: aller dans le tableau tout en bas > choisir une ligne à vérifier > une fenêtre de traitement s'ouvre
+                </li>
+            </ul>
+        </li>
     </ol>
     `
 );
